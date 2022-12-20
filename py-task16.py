6c72bddca620a2fd7e36db6aca7eddb8629a8f19
Alexandr <jwchaganav@gmail.com>
2022-12-20 15:11:14 +0400

    проверил

---
+++ py-task16.py
@@ -0,0 +1,24 @@
+import random
+
+min = 0
+max = 5
+try:
+    # N = 20//2
+    N = int(input("Введите размер массива: "))//2
+    # X = random.randint(min,max)
+    X = int(input("Введите число поиска: "))
+    print(X)
+    numbers = []
+    for num in range(min,N):
+        numbers.append(random.randint(min,max))
+    count = 0
+    print(numbers)
+    if X in numbers:
+        for n in numbers:
+            if n == X:
+                count+=1
+        print(f"Число {X} встречается {count} раз")
+    else:
+        print(f"Число {X} ни разу не встречается")
+except:
+    print("Упс, что-то пошло не так")
